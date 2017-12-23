7b3659639fb1d8809cd034e77b0baddb38c9fc44
Tomek <tomek7sa@gmail.com>
2017-12-23 09:50:16 +0100

    Taking input from user

---
+++ book_program/src/books_program.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : books_program.cpp
+// Author      : Tomek
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Simple book_program
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << " and " << v2
+			<< " is " << v1 + v2 << std::endl;
+
+
+	return 0;
+}
